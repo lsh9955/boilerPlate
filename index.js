b513cdbdc6536edc38c6359f7d4b5a37897aa4fa
lsh9955 <cqe9933@gmail.com>
2021-09-16 16:30:40 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb+srv://lee:cookierun@youtubeclone.lv19k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {})
+  .connect(config.mongoURI, {})
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) => res.send("Hello world!!"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
